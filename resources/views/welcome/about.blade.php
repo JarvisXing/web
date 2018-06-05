109183240aa010713d868e4bd793aa05e4b68f8b
JarvisXing <jarvis_xing@sina.com>
2018-06-05 17:45:05 +0800

    new about route

---
+++ resources/views/welcome/about.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Title </title>
+    </head>
+    
+    <body>
+    文档内容......
+    </body>
+    
+
+</html>
